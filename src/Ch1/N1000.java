7a1df8809b0c8ceab79fdb9182d983e0ad49d433
sumin <sumincathy@naver.com>
2019-03-23 01:36:42 +0900

    test

---
+++ src/Ch1/N1000.java
@@ -0,0 +1,12 @@
+package Ch1;
+
+import java.util.Scanner;
+
+public class N1000 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a + b);
+    }
+}
