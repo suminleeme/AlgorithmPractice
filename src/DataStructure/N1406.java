defd025e70e708df1d124b3bff8df3a49cf1ba13
sumin <sumincathy@naver.com>
2019-03-26 15:40:09 +0900

    Modify N1406
    BufferedReader, BufferedWriter, StringBuilder 사용

--- src/DataStructure/N1406.java
+++ src/DataStructure/N1406.java
@@ -1,25 +1,28 @@
 package DataStructure;
 
-import java.util.Scanner;
+import java.io.*;
 import java.util.Stack;
 
-// scanner로 했더니 시간 초과함
+// Scanner로 했더니 시간 초과, StringBuilder 안써도 초과
+// BufferedWriter 안쓰는게 이 예제에서는 더 빠르고 메모리도 적게 쓴다. 길이가 작아서 그런가보다
+// 쓰면 : 메모리 88792KB, 시간 504ms
+// bw.newLine()을 안쓰면 속도가 늘어난다 ?! 540ms
+// 안쓰면(System.out.println) : 메모리 81068B, 시간 496ms
 
 public class N1406 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Stack<Character> stack1 = new Stack<>();
         Stack<Character> stack2 = new Stack<>();
-        String input = scanner.nextLine();
-        int n = scanner.nextInt();
-        scanner.nextLine();
+        String input = br.readLine().trim();
+        int n = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < input.length(); i++) {
             stack1.push(input.charAt(i));
         }
 
         for (int i = 0; i < n; i++) {
-            String cmd = scanner.nextLine().trim();
+            String cmd = br.readLine();
 
             switch (cmd.charAt(0)) {
                 case 'L':
@@ -46,28 +49,29 @@ public class N1406 {
                     break;
 
                 case 'P':
-                    char push = cmd.charAt(2);
-                    stack1.push(push);
+                    char val = cmd.charAt(2);
+                    stack1.push(val);
                     break;
 
                 default:
                     System.out.println("cmd 오류");
             }
         }
-        String output = "";
-//        StringBuffer output = new StringBuffer("") ;
 
-        for (int j = 0; j < stack1.size(); j++) {
-                output += stack1.get(j);
-//            output.append(stack1.get(j));
+        // StringBuffer(synchronized)보다 StringBuilder(unsynchronized)가 빠르다
+        StringBuilder output = new StringBuilder();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        while (!stack1.empty()) {
+            stack2.push(stack1.pop());
         }
 
-        for (int j = 0; j < stack2.size(); j++) {
-                output += stack2.get(stack2.size() - j - 1);
-//            output.append(stack2.size() - j - 1);
+        while (!stack2.empty()) {
+            output.append(stack2.pop());
         }
-        System.out.println(output);
-
-        scanner.close();
+        bw.write(String.valueOf(output));
+        bw.newLine();
+        bw.flush();
+        br.close();
     }
 }
